c751fbcdcd2896bdb9d9fecfbd5f9a31611b031e
Levi Trejo <ltrejo32@gmail.com>
2023-03-30 12:49:10 -0500

    feat(App): routing pages and layouts, css, components

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import "@/styles/global.css";
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
