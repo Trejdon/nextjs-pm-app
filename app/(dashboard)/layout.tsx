3c882bf35eb02d7f9ad87b921ddcb9c299087a07
Levi Trejo <ltrejo32@gmail.com>
2023-04-17 17:30:48 -0500

    feat(CSS): address homepage padding issues

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
     <html lang="en" className={`${inter.variable}`}>
       <head />
       <body className="h-screen w-screen candy-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center p-2 gap-1">
+        <GlassPane className="w-full h-full flex items-center gap-4">
           <Sidebar />
           {children}
         </GlassPane>
